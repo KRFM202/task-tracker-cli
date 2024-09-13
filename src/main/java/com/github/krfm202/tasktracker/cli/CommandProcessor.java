f8922feb1dfbc0b1872596503d0dd248cfb36314
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-13 16:19:18 -0600

    Refactorización de clase CommandProcessor

---
+++ src/main/java/com/github/krfm202/tasktracker/cli/CommandProcessor.java
@@ -0,0 +1,29 @@
+package com.github.krfm202.tasktracker.cli;
+
+import java.util.List;
+
+public class CommandProcessor {
+
+    private List<String> args;
+
+    CommandProcessor(List<String> args) {
+        this.args = args;
+    }
+
+    public void processAdd() {
+        System.out.println(args);
+        System.out.println("comando ejecutado add");
+    }
+
+    public void processDelete() {
+        System.out.println(args);
+        System.out.println("comando ejecutado delete");
+    }
+
+    public void processListAll() {
+    }
+
+    public void processExit() {
+        System.exit(0);
+    }
+}
