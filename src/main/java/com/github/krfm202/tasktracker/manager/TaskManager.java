1d9416050f738f21289b2883e4dd5db15a6cc9c6
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-13 17:51:25 -0600

    Agregando funcionalidad al metodo add() de la clase TaskManager

--- src/main/java/com/github/krfm202/tasktracker/manager/TaskManager.java
+++ src/main/java/com/github/krfm202/tasktracker/manager/TaskManager.java
@@ -1,4 +1,21 @@
 package com.github.krfm202.tasktracker.manager;
 
+import com.github.krfm202.tasktracker.model.Task;
+import com.github.krfm202.tasktracker.model.TaskStatus;
+
 public class TaskManager {
+
+    private Task task;
+
+    public TaskManager() {
+
+    }
+
+    public void add(String description) {
+        task = new Task();
+        task.setDescription(description);
+        task.setStatus(TaskStatus.TODO);
+    }
+
+
 }
