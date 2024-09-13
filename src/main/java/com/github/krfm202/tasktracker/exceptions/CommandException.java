3e17ade88c121ba0b413a26ca764cb0ba5d8de6b
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-13 16:20:06 -0600

    Agregando nueva excepcion personalizada CommandException

---
+++ src/main/java/com/github/krfm202/tasktracker/exceptions/CommandException.java
@@ -0,0 +1,8 @@
+package com.github.krfm202.tasktracker.exceptions;
+
+public class CommandException extends RuntimeException {
+
+    public CommandException(String message) {
+        super(message);
+    }
+}
