be80c65b9e61963f9ec610f8e37675d0873555ed
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-13 22:53:46 -0600

    Agregando clase Application para el run

---
+++ src/main/java/com/github/krfm202/tasktracker/Application.java
@@ -0,0 +1,9 @@
+package com.github.krfm202.tasktracker;
+
+import com.github.krfm202.tasktracker.cli.CommandInterpreter;
+
+public class Application {
+    public static void main(String[] args) {
+        CommandInterpreter.run();
+    }
+}
