e69b0b5db9fd52c26a6f317ddaf44f90dacd9033
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-15 00:36:29 -0600

    Agregando id y description a la clase enum Status

--- src/main/java/com/github/krfm202/tasktracker/model/Status.java
+++ src/main/java/com/github/krfm202/tasktracker/model/Status.java
@@ -1,5 +1,8 @@
 package com.github.krfm202.tasktracker.model;
 
 public enum Status {
-    TODO, IN_PROGRESS, DONE
+    TODO(1, "To do"), IN_PROGRESS(2, "In progress"), DONE(3, "Done");
+
+    Status(int id, String description) {
+    }
 }
