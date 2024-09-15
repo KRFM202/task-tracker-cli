e575bceeeb21c941c9ce11ed7eb9660d9ed2e796
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-14 22:45:49 -0600

    arreglando el campo id para que sea de formato unico

--- src/main/java/com/github/krfm202/tasktracker/model/Task.java
+++ src/main/java/com/github/krfm202/tasktracker/model/Task.java
@@ -2,22 +2,22 @@ package com.github.krfm202.tasktracker.model;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.UUID;
 
 public class Task {
-    private int id;
+    private final String id;
     private String description;
     private Status status;
-    private LocalDateTime createdAt;
+    private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private static int idAccumulator = 1;
 
     public Task() {
-        this.id = idAccumulator++;
+        this.id = UUID.randomUUID().toString();
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
     }
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
@@ -54,7 +54,7 @@ public class Task {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return id == task.id;
+        return Objects.equals(id, task.id);
     }
 
     @Override
