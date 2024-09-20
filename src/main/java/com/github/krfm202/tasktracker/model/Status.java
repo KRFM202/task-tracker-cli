8fef67971fc6cdee3753fd78104a9da651a45aa8
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-19 23:38:37 -0600

    Refactorizando y agregando description y id a la clase enum Task

--- src/main/java/com/github/krfm202/tasktracker/model/Status.java
+++ src/main/java/com/github/krfm202/tasktracker/model/Status.java
@@ -1,8 +1,22 @@
 package com.github.krfm202.tasktracker.model;
 
 public enum Status {
-    TODO(1, "To do"), IN_PROGRESS(2, "In progress"), DONE(3, "Done");
+    TODO(1, "To do"), IN_PROGRESS(2, "In progress"), DONE(3, "Done"),
+    ALL(4, "");
+
+    private final int id;
+    private final String description;
 
     Status(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
