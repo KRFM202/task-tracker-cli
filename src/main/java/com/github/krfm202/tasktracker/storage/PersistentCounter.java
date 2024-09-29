e62804becad1908b01a0fc010053829d75e9d89f
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-29 00:26:25 -0600

    Refactorizando clase PersistentCounter y arreglando la ruta del contador de task

--- src/main/java/com/github/krfm202/tasktracker/storage/PersistentCounter.java
+++ src/main/java/com/github/krfm202/tasktracker/storage/PersistentCounter.java
@@ -3,10 +3,17 @@ package com.github.krfm202.tasktracker.storage;
 import java.io.*;
 
 public class PersistentCounter implements Serializable {
-    private static final String PATH = "src/main/resources/counter.ser";
+    private static final String USER_HOME = System.getProperty("user.home");
+    private static final String APP_DIRECTORY = USER_HOME + "/.task-tracker-data";
+    private static final String PATH = APP_DIRECTORY + "/counter.ser";
+
     @Serial
     private static final long serialVersionUID = 1L;
     private int counter;
+
+    public PersistentCounter() {
+        ensureFileExist();
+    }
 
     public void increment() {
         counter++;
@@ -26,18 +33,24 @@ public class PersistentCounter implements Serializable {
 
     public static PersistentCounter loadState() {
         File file = new File(PATH);
-        try {
-            if (file.createNewFile()) {
-                return new PersistentCounter();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Error: " + e);
-        }
+        if (!file.exists()) return new PersistentCounter();
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(PATH))) {
             return (PersistentCounter) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("Error: " + e);
             return new PersistentCounter();
+        }
+    }
+
+    private void ensureFileExist() {
+        new File(APP_DIRECTORY).mkdirs();
+        File file = new File(PATH);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException("Error: " + e);
+            }
         }
     }
 }
