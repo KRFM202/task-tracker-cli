7382e55e1d3efc533019254eb28dd35452023b18
krfm202 <53411842+KRFM202@users.noreply.github.com>
2024-09-29 00:11:07 -0600

    Cambiando el nombre de la clase

--- src/main/java/com/github/krfm202/tasktracker/TaskTrackerApp.java
+++ src/main/java/com/github/krfm202/tasktracker/TaskTrackerApp.java
@@ -2,7 +2,7 @@ package com.github.krfm202.tasktracker;
 
 import com.github.krfm202.tasktracker.cli.CommandInterpreter;
 
-public class Application {
+public class TaskTrackerApp {
     public static void main(String[] args) {
         CommandInterpreter commandInterpreter = new CommandInterpreter();
         commandInterpreter.run();
